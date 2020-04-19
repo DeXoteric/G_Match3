829603c0a80795dcb6db0efe9ef15dc6e2098978
DeXoteric <dexoteric@gmail.com>
2020-04-19 21:38:21 +0200

    Taft Episode 24.1

--- G_Match3/scripts/Piece.gd
+++ G_Match3/scripts/Piece.gd
@@ -1,8 +1,14 @@
 extends Node2D
 
 export var color: String
+export var row_texture: Texture
+export var column_texture: Texture
+export var adjacent_texture: Texture
 
 var matched := false
+var is_row_bomb := false
+var is_column_bomb := false
+var is_adjacent_bomb := false
 
 onready var move_tween = $MoveTween
 
@@ -10,6 +16,24 @@ onready var move_tween = $MoveTween
 func move(target):
 	move_tween.interpolate_property(self, "position", position, target, 0.3, Tween.TRANS_SINE, Tween.EASE_OUT)
 	move_tween.start()
+
+
+func make_column_bomb():
+	is_column_bomb = true
+	$Sprite.texture = column_texture
+	$Sprite.modulate = Color(1, 1, 1, 1)
+
+
+func make_row_bomb():
+	is_row_bomb = true
+	$Sprite.texture = row_texture
+	$Sprite.modulate = Color(1, 1, 1, 1)
+
+
+func make_adjacent_bomb():
+	is_adjacent_bomb = true
+	$Sprite.texture = adjacent_texture
+	$Sprite.modulate = Color(1, 1, 1, 1)
 
 
 func dim():
